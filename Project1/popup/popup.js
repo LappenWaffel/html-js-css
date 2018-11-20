77ba82d5bfe7fb9b6cdccd1dee63fe48db100237
Uhrendoktor <docclock85@gmail.com>
2018-11-20 21:19:55 +0100

    added popup
    ...

---
+++ Project1/popup/popup.js
@@ -0,0 +1,11 @@
+$(function Test(width, height, information) {
+	var body = document.createElement("div");
+	body.className = "popup";
+	body.style.width = `${width}px`;
+	body.style.height = `${height}px`;
+	var img = document.createElement("div");
+	img.style.backgroundImage = information[0];
+	var text = document.createElement("div");
+	var span = 
+	document.getElementById("Popup");
+});

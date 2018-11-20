677404fbe1d80d44a1ca2e09ce5f485c1bf5a600
Uhrendoktor <docclock85@gmail.com>
2018-11-20 22:55:43 +0100

    added Popups
    Added complex Popup class with examples in the index.js

--- Project1/Index.js
+++ Project1/Index.js
@@ -4,26 +4,17 @@ function hello () {
 
 function OnLoad() {
 	CreateMenu({
-			0:{
-				0: "Jacob", 1: Lul
-			},
-			1:{
-				0: "Julius", 1:change
-			},
-			2: {
-				0: "Lappen", 1: change
-			}
-		});
+		0: {
+			0: "Jacob", 1: Lul
+		},
+		1: {
+			0: "Julius", 1: change
+		},
+		2: {
+			0: "Lappen", 1: change
+		}
+	});
 	CreateCookies();
-	Test();
-}
-
-function Lul() {
-	alert("...");
-}
-
-function Lal () {
-	alert("This is what we wanted froma young age!");
 }
 
 count = 0;
@@ -44,3 +35,42 @@ lines = [
 		"workin. "
 	}
 ]
+
+
+//Examples for Popups
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//vars			: Popup.width, Popup.height, Popup.positionX, Popup,positionY, Popup.information.text, Popup.information.img, Popup.information.onclick
+//DOM-Elements	: Popup.body, Popup.img, Popup.text, Popup.exitBtn
+//functions		: Popup.destruct, Popup.setSize(width, height), Popup.setPosition(positionX, positionY), Popup.setText(text), setImage(path)
+
+//1.
+function CreatePopup() {
+	var popup = new Popup(/*width*/120, /*height*/300, /*positionX*/150, /*positionY*/50, /*information*/{ text: "You Won! Click here!", img: "/popup/popup.jpg", onclick=OnPopupClick });
+
+	//just for better visualisation:
+	var popup2 = new Popup(/*width*/    120,
+						   /*height*/   300,
+						   /*positionX*/150,
+						   /*positionY*/ 50,
+						   /*information*/ {
+							   text: "You Won! Click here!",
+							   img: "/popup/popup.jpg",
+							   onclick=OnPopupClick
+						   });				 // | //
+}											 // | //
+function OnPopupClick() {					 // v //
+	alert("Your price will be send to you until next century!");
+}
+
+//2.
+function CreatePopup2() {
+	var popup = new Popup(/*width*/120, /*height*/300, /*positionX*/150, /*positionY*/50, /*information*/{
+		text: "You Won! Click here!",
+		img: "/popup/popup.jpg",
+		onclick=function () {
+			alert("Your price doesnt exist! Weve nevwe thaught that anyone is dump enough to click on this popup!");
+		}
+	});
+}
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
--- Project1/popup/popup.js
+++ Project1/popup/popup.js
@@ -1,11 +1,67 @@
-$(function Test(width, height, information) {
-	var body = document.createElement("div");
-	body.className = "popup";
-	body.style.width = `${width}px`;
-	body.style.height = `${height}px`;
-	var img = document.createElement("div");
-	img.style.backgroundImage = information[0];
-	var text = document.createElement("div");
-	var span = 
-	document.getElementById("Popup");
-});
+class Popup {
+	constructor(width, height, positionX, positionY, information) {
+		this.width = width;
+		this.height = height;
+		this.information = information;
+		this.PositionX = positionX;
+		this.positionY = positionY;
+
+		this.body = document.createElement("div");
+		this.body.className = "popup";
+		this.body.style.width = `${width}px`;
+		this.body.style.height = `${height}px`;
+		this.body.onclick = information.onclick;
+		this.body.style.left = `${positionX}px`;
+		this.body.style.bottom = `${positionY}px`;
+
+		this.img = document.createElement("div");
+		this.img.className = "_img";
+		if (information.img != undefined) {
+			this.img.style.backgroundImage = `url(${information.img})`;
+		}
+		this.text = document.createElement("div");
+		this.text.className = "_text";
+
+		var span = document.createElement("span");
+		span.textContent = information.text;
+
+		this.exitBtn = document.createElement("div");
+		this.exitBtn.className = "_exit";
+		this.exitBtn.onclick = function () {
+			this.destruct();
+		}.bind(this);
+
+		this.text.append(span);
+		this.body.append(this.img);
+		this.body.append(this.text);
+		this.body.append(this.exitBtn);
+
+		document.getElementById("Popup").append(this.body);
+	}
+
+	setPosition(PositionX, PositionY) {
+		this.PositionX = positionX;
+		this.positionY = positionY;
+		this.body.style.left = `${positionX}px`;
+		this.body.style.bottom = `${positionY}px`;
+	}
+
+	setSize(width, height) {
+		this.width = width;
+		this.height = height;
+		this.body.style.width = `${width}px`;
+		this.body.style.height = `${height}px`;
+	}
+
+	setText(text) {
+		this.text.firstChild.textContent = this.information.text = text;
+	}
+
+	setImage(path) {
+		this.img.backgroundImage = this.information.img = `url(${path})`;
+	}
+
+	destruct() {
+		this.body.parentElement.removeChild(this.body);
+	}
+}
